90f0874fd71b0f14e3fbc1ae3f9e8aa5b9ffa460
Dirk Songür <Dirk.Songuer@razorfish.de>
2015-08-04 00:41:32 +0200

    Lobby create and join optimisation

--- classes/lobbyhandler.js
+++ classes/lobbyhandler.js
@@ -7,7 +7,11 @@
 // instances.
 // *************************************************** //
 
+// UUID
 var uuid = require('node-uuid');
+
+// game handler
+var gameHandler = require('./gamehandler.js');
 
 var lobbyHandler = new LobbyhandlerClass();
 
@@ -16,6 +20,7 @@ function LobbyhandlerClass() {
 	this.lobbyStorage = new Array();
 }
 
+// create a new lobby
 LobbyhandlerClass.prototype.createLobby = function (sessionId) {
 	console.log("# Creating a new lobby");
 
@@ -32,6 +37,7 @@ LobbyhandlerClass.prototype.createLobby = function (sessionId) {
 	return newLobby.lobbyId;
 }
 
+// join an already existing lobby
 LobbyhandlerClass.prototype.joinLobby = function (sessionId, lobbyId) {
 	console.log("# Joining lobby with id " + lobbyId);
 	
@@ -39,7 +45,7 @@ LobbyhandlerClass.prototype.joinLobby = function (sessionId, lobbyId) {
 	var lobbyPos = this.lobbyStorage.map(function (x) { return x.lobbyId; }).indexOf(lobbyId);
 
 	// no matching lobby found
-	if (!lobbyPos) {
+	if (lobbyPos < 0) {
 		return false;
 	}
 
@@ -50,6 +56,34 @@ LobbyhandlerClass.prototype.joinLobby = function (sessionId, lobbyId) {
 	return true;
 }
 
+// confirm that the game can start
+// the game will start once all participants in a lobby have confirmed
+LobbyhandlerClass.prototype.confirmLobby = function (sessionId, lobbyId) {
+	console.log("# Confirming lobby with id " + lobbyId);
+	
+	// find index of a lobby with respective id
+	var lobbyPos = this.lobbyStorage.map(function (x) { return x.lobbyId; }).indexOf(lobbyId);
+
+	// no matching lobby found
+	if (lobbyPos < 0) {
+		return false;
+	}
+	
+	// check if the session is really in the lobby
+	if (this.lobbyStorage[lobbyPos].lobbyParticipants.indexOf(sessionId) < 0) {
+		return false;
+	}	
+
+	// add session to participants confirmed list
+	this.lobbyStorage[lobbyPos].lobbyParticipantsConfirmed.push(sessionId);
+
+	// check if all participants have already confirmed	
+	if (this.lobbyStorage[lobbyPos].lobbyParticipantsConfirmed.length == this.lobbyStorage[lobbyPos].lobbyParticipants.length) {
+	}
+
+	console.log("# Session " + sessionId + " confirmed lobby " + lobbyId + ", " + this.lobbyStorage[lobbyPos].lobbyParticipantsConfirmed.length + " / " + this.lobbyStorage[lobbyPos].lobbyParticipants.length);
+	return true;
+}
 
 // Reference object for a game
 function LobbyObject() {
@@ -58,6 +92,9 @@ function LobbyObject() {
 
 	// contains the session ids of lobby participants
 	this.lobbyParticipants = new Array();
+
+	// contains the session ids of participants that already confirmed
+	this.lobbyParticipantsConfirmed = new Array();
 
 	// global state for this lobby
 	// note: this is NOT the game state for a user!
---
+++ server_modules/lobby/confirm.js
@@ -0,0 +1,13 @@
+
+// lobby handler
+var lobbyHandler = require('../../classes/lobbyhandler.js');
+
+var run = function (sender, data) {
+	// confirm the lobby participation
+	var state = lobbyHandler.confirmLobby(sender.id, data);
+		
+	// done
+	return state;
+};
+
+module.exports = run;
