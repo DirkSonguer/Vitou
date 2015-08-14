46a5221653e4ba98763b6ae8970b760213ad10c3
Dirk Songür <Dirk.Songuer@razorfish.de>
2015-08-14 20:31:41 +0200

    Added successors + configurable game directories

--- classes/eventhandler.js
+++ classes/eventhandler.js
@@ -75,19 +75,20 @@ EventhandlerClass.prototype.executeEvent = function (sender, event) {
 	// TODO: OMFGWTF SECURITY!!!!
 	
 	// choose respective directory based on event type
-	var eventDirectory = 'server_modules';
-	if (event.type == 'game') {
-		eventDirectory = 'game_modules';
+	var eventDirectory = configurationHandler.configurationStorage.server.gameDirectory;
+	if (event.type == 'system') {
+		// checking if server module is active
+		if (configurationHandler.configurationStorage.server.activeModules.indexOf(event.module) == -1) {
+			logHandler.log('Event calls a module that is on the list of private server directories: ' + event.module, 3);
+			return false
+		}
+
+		// set server modules dir
+		eventDirectory = 'server_modules';
 	}
 	
 	// build path to event source file
 	var eventSourcePath = filePath.join(__dirname, '/../' + eventDirectory + '/' + event.module + '/' + event.action + '.js');
-
-	// checking if module is on the blacklist
-	if (configurationHandler.configurationStorage.server.privateServerDirectories.indexOf(event.module) != -1) {
-		logHandler.log('Event calls a module that is on the list of private server directories: ' + event.module, 3);
-		return false
-	}
 
 	// checking if the module and action actually exists in the system
 	try {
@@ -109,7 +110,7 @@ EventhandlerClass.prototype.executeEvent = function (sender, event) {
 		if ((event.module == 'session') && (event.action == 'connect')) {
 			session = sender;
 		} else {
-			logHandler.log('Could not find a valid session for id ' + sender.id, 2);
+			logHandler.log('Could not find a valid session for id ' + sender.id, 3);
 			return false;
 		}
 	}	
@@ -119,8 +120,29 @@ EventhandlerClass.prototype.executeEvent = function (sender, event) {
 	var eventAction = require(eventSourcePath);
 	var eventResult = eventAction(session, event.data);
 
+	// check for relevant hooks
+	var eventString = event.type + '/' + event.module + '/' + event.action;
+	var relevantSuccessors = configurationHandler.configurationStorage.successors.filter(function (el) {
+		return el.if == eventString;
+	});
+
+	// done
 	if (!eventResult) {
 		logHandler.log('# The call ' + eventSourcePath + ' with data ' + event.data + ' went wrong. INVESTIGATE!!!!', 3);
+	}
+
+	// if successors were found, execute the first one
+	if (relevantSuccessors.length > 0) {
+		logHandler.log('Found successor for ' + eventString + ', calling ' + relevantSuccessors[0].then, 2);
+		var successorEventArray = relevantSuccessors[0].then.split('/');
+		if (successorEventArray.length != 3) {
+			logHandler.log('Successor has wrong format: ' + relevantSuccessors[0].then, 3);
+			return false;
+		}
+		
+		// build and execute successor event
+		var successorEvent = eventHandler.createEventObject(successorEventArray[0], successorEventArray[1], successorEventArray[2], eventResult);
+		eventHandler.executeEvent(session, successorEvent);
 	}
 }
 
