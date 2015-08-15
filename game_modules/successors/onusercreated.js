ef2186a051c860738d6c871d7a09503339fdac90
Dirk Songür <Dirk.Songuer@razorfish.de>
2015-08-15 19:27:45 +0200

    Updated initial game related user data

--- game_modules/successors/onusercreated.js
+++ game_modules/successors/onusercreated.js
@@ -8,6 +8,18 @@ var gamedataHandler = require('../../classes/gamedatahandler.js');
 var run = function (session, data) {
 	// set initial data structure for the new user
 	var userData = gamedataHandler.gameStructures.user;
+	
+	// import initial tank
+	var defaultTank = gamedataHandler.getGameDataByAssemblage('tank');
+	userData.garage.push(defaultTank[0].id);
+	userData.activeTank = defaultTank[0].id
+	
+	// import initial weaponturret
+	var defaultWeaponTurret = gamedataHandler.getGameDataByAssemblage('weaponturret');
+	userData.garage.push(defaultWeaponTurret[0].id);
+	userData.weaponTurret = defaultWeaponTurret[0].id
+		
+	// store data in user object
 	userHandler.updateUserData(session.user, userData);
 	
 	// done
